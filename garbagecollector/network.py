63ebcb4b0ff0a1277d6db9b0c43aabf73bdba95e
Ilya Stepanov <istepanov@renderedsource.com>
2012-09-10 21:06:55 +0700

    hotfix: gc fails if mac addresses are unavailable

--- garbagecollector/network.py
+++ garbagecollector/network.py
@@ -4,18 +4,22 @@ from website.settings import NMAP_XML_URL
 
 #todo cache 1-5 minuts
 def get_online_mac_addesses():
-    response = urllib.urlopen(NMAP_XML_URL)
-    xml = response.read()
-    tree = ElementTree.fromstring(xml)
-
     mac_addresses = []
 
-    for host in tree.findall('host'):
-        state = host.find('status').attrib.get('state')
-        if state == 'up':
-            for address in host.findall('address'):
-                if address.attrib.get('addrtype') == 'mac':
-                    mac = address.attrib.get('addr')
-                    mac_addresses.append(mac)
+    try:
+        response = urllib.urlopen(NMAP_XML_URL)
+        xml = response.read()
+        tree = ElementTree.fromstring(xml)
+
+        for host in tree.findall('host'):
+            state = host.find('status').attrib.get('state')
+            if state == 'up':
+                for address in host.findall('address'):
+                    if address.attrib.get('addrtype') == 'mac':
+                        mac = address.attrib.get('addr')
+                        mac_addresses.append(mac)
+                        
+    except IOError:
+        pass
 
     return mac_addresses
