7840aecba663388dc76076618d50f360186fe348
Alexey Baev <abaev@renderedsource.com>
2012-09-11 09:28:45 +0700

    Fix network.

--- garbagecollector/network.py
+++ garbagecollector/network.py
@@ -6,18 +6,20 @@ from website.settings import NMAP_XML_URL
 def get_online_mac_addesses():
     try:
         response = urllib.urlopen(NMAP_XML_URL)
+
+
+        xml = response.read()
+        tree = ElementTree.fromstring(xml)
+
+        mac_addresses = []
+
+        for host in tree.findall('host'):
+            state = host.find('status').attrib.get('state')
+            if state == 'up':
+                for address in host.findall('address'):
+                    if address.attrib.get('addrtype') == 'mac':
+                        mac = address.attrib.get('addr')
+                        mac_addresses.append(mac)
     except :
-        response =[]
-    xml = response.read()
-    tree = ElementTree.fromstring(xml)
-
-    mac_addresses = []
-
-    for host in tree.findall('host'):
-        state = host.find('status').attrib.get('state')
-        if state == 'up':
-            for address in host.findall('address'):
-                if address.attrib.get('addrtype') == 'mac':
-                    mac = address.attrib.get('addr')
-                    mac_addresses.append(mac)
+        mac_addresses = []
     return mac_addresses
