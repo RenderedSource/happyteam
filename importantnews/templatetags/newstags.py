cbb7d01fb879cc8fac62319ef03f307da2c69dd6
Alexey Baev <abaev@renderedsource.com>
2012-09-13 10:14:50 +0700

    Add last comments tag

--- importantnews/templatetags/newstags.py
+++ importantnews/templatetags/newstags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.auth.models import User
 import datetime
+from django.contrib.comments.models import Comment
 from importantnews.models import News
 
 register = template.Library()
@@ -9,3 +10,9 @@ __author__ = 'lehabaev'
 @register.inclusion_tag('tags/latest_news.html')
 def latest_news(num):
     return {'news_list': News.objects.all().order_by('-required', '-date')[:num]}
+
+@register.inclusion_tag('tags/last_comments.html')
+def last_comments(num):
+    comment_list = Comment.objects.all().order_by('-submit_date')[:num]
+
+    return {"comment_list":comment_list}
