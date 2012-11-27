d7b1ff974ca8bcaa4c407562aa18e5c02f1f58e4
Alexey Baev <abaev@renderedsource.com>
2012-11-27 11:34:34 +0700

    Alert unread news

--- importantnews/templatetags/newstags.py
+++ importantnews/templatetags/newstags.py
@@ -2,8 +2,7 @@ from django import template
 from django.contrib.auth.models import User
 import datetime
 from django.contrib.comments.models import Comment
-from importantnews.models import News
-
+from importantnews.models import News, UserRead
 register = template.Library()
 __author__ = 'lehabaev'
 
@@ -16,3 +15,10 @@ def last_comments(num):
     comment_list = Comment.objects.all().order_by('-submit_date')[:num]
 
     return {"comment_list":comment_list}
+
+
+@register.inclusion_tag('tags/unread_news.html')
+def unread_news(user_id):
+    read_news = UserRead.objects.filter(user__id=user_id).values_list('news__id', flat=True)
+    news_count = News.objects.all().exclude(id__in=read_news).order_by('-required', '-date').count()
+    return {"news_count":news_count}
