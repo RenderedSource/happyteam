b4c77b29885ddf5875e913d732e285bda671a7fd
Alexey Baev <abaev@renderedsource.com>
2013-01-21 09:52:05 +0700

    Js

--- static/js/buglist.js
+++ static/js/buglist.js
@@ -1,13 +1,16 @@
 $().ready(function(){
-    var user_id = 1;
+
     $('.nav-buttons').button('toggle');
     $('#my-work').on('click', function(){
+        var user_id = $(this).data('user_id');
         $.get('',{'requester':user_id});
     });
     $('#my-bugs').on('click', function(){
+        var user_id = $(this).data('user_id');
         $.get('',{'owner':user_id});
     });
     $('#resolved').on('click', function(){
+        var user_id = $(this).data('user_id');
         $.get('',{'status':true});
     });
 });
